fdfd1eb38eb73f9177ab38fd93cae5589d6de532
Kaatarzyna <k.duchowska@o2.pl>
2017-04-19 17:27:57 +0200

    podłączanie graczy

--- src/main/Serwer.java
+++ src/main/Serwer.java
@@ -12,9 +12,12 @@ import java.awt.Point;
 
 public class Serwer {
 
+	private final int ilosc_graczy = 2;
     private static final int PORT = 1306;
     private final ExecutorService executor = Executors.newFixedThreadPool(4);
     private ArrayList<Players> players;
+    private static int zalogowani = 0;
+    
     
     private final int[] posX = {50, 650, 50, 650};
     private final int[] posY = {50, 50, 550, 550};
@@ -23,22 +26,11 @@ public class Serwer {
     public Serwer() { 	
     	System.out.println("Serwer jedzie");
     	players = new ArrayList();
-    	/*
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            while (true) {
-                final Socket socket = serverSocket.accept();
-                executor.submit(() -> makeConnection(socket));
-            }
-        } catch (IOException x) {
-            System.out.println("Wyj¹tek I/O");
-        } catch (Exception e) {
-            System.out.println("Nie uda³o siê po³¹czyæ");
-        }
-        */
+    	
     	try (ServerSocket serverSocket = new ServerSocket(PORT)) {
     		while(true) {
     			final Socket socket = serverSocket.accept();
-                executor.submit(() -> makeConnection(socket));
+    			executor.submit(() -> makeConnection(socket, zalogowani));
     		}
         } catch (IOException x) {
             System.out.println("Wyj¹tek I/O");
@@ -48,49 +40,45 @@ public class Serwer {
     	
     }
 
-    private void makeConnection(Socket socket) {
+   
+   private void makeConnection(Socket socket, int logged) {
         try {
-
-            	ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-            	int option = (int) in.readObject();
-            	
-                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-                switch (option) {
-                case 1:
-                	//zalogowal sie      	
-                	if (players.size() < 4) {
-                		players.add(new Players(posX[players.size()], posY[players.size()]));
-                		out.writeObject(posX[players.size() - 1]);
-                		out.writeObject(posY[players.size() - 1]);
+        	ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                
+            //zalogowal sie      	
+            if (players.size() < ilosc_graczy) {
+                players.add(new Players(posX[players.size()], posY[players.size()]));
+                out.writeObject(posX[players.size() - 1]);
+                out.writeObject(posY[players.size() - 1]);
                 		
-                		while (players.size() != 4) {
-                		out.writeObject(players.size() - 1);
-                		for (int i=0; i < players.size() - 1; i++) {
-                			out.writeObject(players.get(i).getPos().x);
-                			out.writeObject(players.get(i).getPos().y);
+                synchronized(executor) {
+                	try {
+                		zalogowani++;
+                		while(zalogowani < ilosc_graczy) {
+                			executor.wait();
                 		}
-                		}
-               		} 
-               		else {
-               			out.writeObject(0);
-               		}
-                	
-                	break;
-                case 2:
-                	// postawil bombe
-                	
-                	
-                	break;
-                case 3:
-                	// zginal
-                	
-                	
-                	break;
+                		if (logged == ilosc_graczy-1) executor.notifyAll();
+                		} catch (InterruptedException e) {
+                			e.printStackTrace();
+                	}
                 }
-                in.close();
-                out.close();
+                out.writeObject(players.size() - 1);
+                for (int i=0; i < players.size(); i++) {
+                	if (i!=logged) {
+                		out.writeObject(players.get(i).getPos().x);
+                		out.writeObject(players.get(i).getPos().y);
+                	}
+                }
+                		
+            } 
+            else {
+               	out.writeObject(0);
+            } 	
+                	
+            out.close();
         } catch (Exception e) {
             System.out.println("Wyj¹tek I/O - metoda odbierz w serwerze");
+            e.printStackTrace();
             System.out.println(e.getMessage());
         } finally {
             try {
@@ -100,5 +88,6 @@ public class Serwer {
             }
         }
     }
+    
 
 }
